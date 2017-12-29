acd29f92eaf2297b0a7cd0210a6e68a557cadc37
Lucas Machado <lucas.dpmachado@gmail.com>
2017-12-29 14:44:02 -0200

    Add new Accounts component * Attach accounts user form to header * Update accounts form buttons style

--- client/components/header.js
+++ client/components/header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Accounts from './accounts';
+
 class Header extends Component {
   render() {
     return (
@@ -9,7 +11,7 @@ class Header extends Component {
         </div>
         <ul className="nav navbar-nav">
           <li>
-            <a>Sign Up</a>
+            <Accounts />
           </li>
           <li>
             <a>Create Bin</a>

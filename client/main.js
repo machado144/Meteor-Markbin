b17187d2f1922aeed08e62a586018c07acf229f5
Lucas Machado <lucas.dpmachado@gmail.com>
2017-12-31 14:49:18 -0200

    Update project with new react-router-dom package * Add new remove bins MeteorMethods from BinList * Add new button with onBinRemove event * Update list-group-item css

--- client/main.js
+++ client/main.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import { Bins } from '../imports/api/bins';
 
+const routes = (
+  <BrowserRouter>
+    <Route path="/" component={App}>
+    </Route>
+  </BrowserRouter>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.container'));
+  ReactDOM.render(routes, document.querySelector('.container'));
 });
